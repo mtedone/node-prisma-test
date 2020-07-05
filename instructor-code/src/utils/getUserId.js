b4fc48f070297457cdaba2b9d4222f165a8ae275
Marco Tedone <marco.tedone@gmail.com>
2020-07-05 14:26:57 +0100

    Lock Query Part 1

--- instructor-code/src/utils/getUserId.js
+++ instructor-code/src/utils/getUserId.js
@@ -1,14 +1,16 @@
 import jwt from 'jsonwebtoken';
 
-const getUserId = (request) => {
+const getUserId = (request, requireAuth = true) => {
   const header = request.request.headers.authorization;
-  if (!header) {
-    throw new Error('Authentication required');
+  if (header) {
+    const token = header.replace('Bearer ', '');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    return decoded.userId;
   }
-
-  const token = header.replace('Bearer ', '');
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  return decoded.userId;
+  if (requireAuth) {
+    throw new Error('Authentication Required');
+  }
+  return null;
 };
 
 export { getUserId as default };
