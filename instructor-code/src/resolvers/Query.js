a3fa573a14df882beaa7003d5e3a01ef77c3362f
Marco Tedone <marco.tedone@gmail.com>
2020-07-05 22:38:37 +0100

    Added sorting

--- instructor-code/src/resolvers/Query.js
+++ instructor-code/src/resolvers/Query.js
@@ -2,7 +2,12 @@ import getUserId from '../utils/getUserId';
 
 const Query = {
   users(parent, args, { prisma }, info) {
-    const opArgs = {};
+    const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+      orderBy: args.orderBy,
+    };
 
     if (args.query) {
       opArgs.where = {
@@ -18,6 +23,10 @@ const Query = {
   },
   posts(parent, args, { prisma }, info) {
     const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+      orderBy: args.orderBy,
       where: {
         published: true,
       },
@@ -39,6 +48,10 @@ const Query = {
   myPosts(parent, args, { prisma, request }, info) {
     const userId = getUserId(request);
     const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+      orderBy: args.orderBy,
       where: {
         author: {
           id: userId,
@@ -58,7 +71,16 @@ const Query = {
     return prisma.query.posts(opArgs, info);
   },
   comments(parent, args, { prisma }, info) {
-    return prisma.query.comments(null, info);
+    let opArgs = {};
+    if (args) {
+      opArgs = {
+        first: args.first,
+        skip: args.skip,
+        after: args.after,
+      };
+    }
+
+    return prisma.query.comments(opArgs, info);
   },
   me(parent, args, { prisma, request }, info) {
     const userId = getUserId(request);
